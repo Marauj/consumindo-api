93e2d6486ab5c465ebd2dbb02bbb8fc8f394db19
Jose Neto <maraujneto@gmail.com>
2021-04-09 12:30:01 -0300

    versao 3

--- js/consumindo-api.js
+++ js/consumindo-api.js
@@ -2,13 +2,10 @@ function ajax() {
     senha   = "ipwise@9587";
     usuario = "apiWS";
     $.ajax({
-        url: "https://b1.ativy.com:9804//B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10 HTTP/1.1", 
-        Authorization: "Basic YXBpV1M6aXB3aXNlQDk1ODc=> Accept: */*",
+        url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10", 
         type: "POST",
-        cache: false,
-        crossDomain: true,
         dataType: "json",
-        data: {username: usuario, password: senha},
+        data: {password: senha, username: usuario},
         success: function (result) {
             console.log(result);
         },
