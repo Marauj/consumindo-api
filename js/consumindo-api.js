39f122a1987786ee5b7fd19ae3445b1a8b4c15aa
Jose Neto <maraujneto@gmail.com>
2021-04-09 12:33:58 -0300

    versao 4.1

--- js/consumindo-api.js
+++ js/consumindo-api.js
@@ -3,7 +3,7 @@ function ajax() {
     usuario = "apiWS";
     $.ajax({
         url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10", 
-        headers: {
+        header: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         type: "POST",
