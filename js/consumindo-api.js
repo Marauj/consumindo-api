3da0920c01163d9eb38aa1ad54a098e1cbe55e5f
Jose Neto <maraujneto@gmail.com>
2021-04-09 12:36:21 -0300

    versao 7

--- js/consumindo-api.js
+++ js/consumindo-api.js
@@ -1,46 +1,46 @@
-function ajax() {
-    senha   = "ipwise@9587";
-    usuario = "apiWS";
-    $.ajax({
-        url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10 HTTP/1.1", 
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        type: "POST",
-        dataType: "json",
-        data: {password: senha, username: usuario},
-        success: function (result) {
-            console.log(result);
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });   
-}
-
-ajax();
-
-// function RequestService() {
+// function ajax() {
 //     senha   = "ipwise@9587";
 //     usuario = "apiWS";
 //     $.ajax({
+//         url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10", 
+//         header: {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         },
 //         type: "POST",
-//         url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10",
-//         contentType: "application/json; charset=utf-8",
-//         data: {password: senha, username: usuario},
 //         dataType: "json",
-//         success: function(data) { SucessCallback(data.d); },
-//         error: function(data) { FailureCallBack(data); }
-//     });
+//         data: {password: senha, username: usuario},
+//         success: function (result) {
+//             console.log(result);
+//         },
+//         error: function (error) {
+//             console.log(error);
+//         }
+//     });   
 // }
 
-// RequestService();
+// ajax();
 
-// function SucessCallback(result) {
-//     $('p').html('Resultado: ' + result.Message
-//     + ' <br /> Descrição: ' + result.Description);
-// }
+function RequestService() {
+    senha   = "ipwise@9587";
+    usuario = "apiWS";
+    $.ajax({
+        type: "POST",
+        url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10",
+        contentType: "application/json; charset=utf-8",
+        data: {password: senha, username: usuario},
+        dataType: "json",
+        success: function(data) { SucessCallback(data.d); },
+        error: function(data) { FailureCallBack(data); }
+    });
+}
 
-// function FailureCallBack(result) {
-//     alert(result.status + ' ' + result.statusText);
-// }
+RequestService();
+
+function SucessCallback(result) {
+    $('p').html('Resultado: ' + result.Message
+    + ' <br /> Descrição: ' + result.Description);
+}
+
+function FailureCallBack(result) {
+    alert(result.status + ' ' + result.statusText);
+}
