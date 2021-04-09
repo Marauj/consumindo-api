8cc4446f55a37774e12c6a44729273211d5de956
Jose Neto <maraujneto@gmail.com>
2021-04-09 12:38:55 -0300

    versao 9

--- js/consumindo-api.js
+++ js/consumindo-api.js
@@ -30,7 +30,7 @@ function RequestService() {
         type: "POST",
         url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10",
         contentType: "application/json; charset=utf-8",
-        data: {password: senha, username: usuario},
+        data: {username: usuario, password: senha},
         dataType: "json",
         success: function(data) { SucessCallback(data.d); },
         error: function(data) { FailureCallBack(data); }
