0d66739acd81126d8368db843ffddbc055f69c99
Jose Neto <maraujneto@gmail.com>
2021-04-09 12:43:22 -0300

    versao 4.2

--- js/consumindo-api.js
+++ js/consumindo-api.js
@@ -25,11 +25,16 @@ function RequestService() {
     usuario = "apiWS";
     $.ajax({
         header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Transfer-Encoding':	'chunked',
+            'Authorization': 'Basic YXBpV1M6aXB3aXNlQDk1ODc=> Accept: */*',
+             'Set-Cookie':	'JSESSIONID=82E46233E98017C056F531BCF1B16986; Path=/B1iXcellerator; Secure; HttpOnly',
+             'X-FRAME-OPTIONS':	'SAMEORIGIN',
         },
         type: "POST",
         url: "https://b1.ativy.com:9804/B1iXcellerator/exec/ipo/vP.0010000102.in_HCSX/com.sap.b1i.vplatform.runtime/INB_HT_CALL_SYNC_XPT/INB_HT_CALL_SYNC_XPT.ipo/proc/BuscaITM?pageNumber=1&rowsPerCall=10",
         contentType: "application/json; charset=utf-8",
+        
         data: {username: usuario, password: senha},
         dataType: "json",
         success: function(data) { SucessCallback(data.d); },
